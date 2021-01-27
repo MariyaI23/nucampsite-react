e74a970772881ac268565793308df37d1d8b45c3
Mariya Ivanova <m80ivanova@gmail.com>
2021-01-26 20:54:59 -0600

    Components Part 2

--- src/components/DirectoryComponent.js
+++ src/components/DirectoryComponent.js
@@ -1,57 +1,61 @@
 import React, { Component } from "react";
+import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle } from 'reactstrap';
 
 class Directory extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            campsites: [
-                {
-                    id: 0,
-                    name: 'React Lake Campground',
-                    image: 'assets/images/react-lake.jpg',
-                    elevation: 1233,
-                    description: "Nestled in the foothills of the Chrome Mountains, this campground on the shores of the pristine React Lake is a favorite for fly fishers."
-                },
-                {
-                  id: 1,
-                  name: 'Chrome River Campground ',
-                  image: 'assets/images/chrome-river.jpg',
-                  elevation: 877,
-                  description: "Spend a few sunny days and starry nights beneath a canopy of old-growth firs at this enchanting spot by the Chrome River."
-                },
-                {
-                    id: 2,
-                    name: 'Breadcrumb Trail Campground',
-                    image: 'assets/images/breadcrumb-trail.jpg',
-                    elevation: 2901,
-                    description: "Let NuCamp be your guide to this off-the-beaten-path, hike-in-only campground."
-                },
-                {
-                    id: 3,
-                    name: 'Redux Woods Campground',
-                    image: 'assets/images/redux-woods.jpg',
-                    elevation: 42,
-                    description: "You'll never want to leave this hidden gem, deep within the lush Redux Woods."
-                }
-            ],
+           selectedCampsite: null                
         };
     }
-
-    render() {
-        const directory = this.state.campsites.map(campsite => {
+     /*Above we are establishing a property selected Camspite that will keep track of the last card the user clicked on. The intial value is set to null..*/
+     /*Bellow we are setting up a method that will fire up when the user selects a campsite. We are passing the campsites object. Inside this method we will update the state property with setState() method. Inside it we'll update the selectedCampsite property that we established above from null to now the campsite object which was passed into the onCamspiteSelect() method */
+    onCampsiteSelect(campsite) {
+        this.setState({selectedCampsite: campsite});
+    }
+    /*The onCampsiteSelect() method will be triggered in the Card component bellow with an onClick event since we are trying to track wich Card the user clicks on. */
+    /*Bellow we will establish a new method after the user has selected the Card he wants to view addl info on. This method will render(display) the new card with more info. */
+    /*Here with the if statement we are checking to see if campsite is an object, if yes it will evaluate as truthy, however if campsite is null or undefined it will be falsy. If truthy it will display the chosen card to the user WITH the addl information(the description property) */
+    /*The renderSelectedCampsite() method will be called in the final return statement at the end of this whole component. */
+    renderSelectedCampsite(campsite) {
+        if(campsite) {                    
             return (
-                <div key = {campsite.id} className = "col">           {/*Here we are using key so we can identify each item in the array of objects by it's unic id. The app will work fine without it but it gives warning in the console and we add it to follow best practices*/}
-                    <img src = {campsite.image} alt = {campsite.name} />        {/*We are using curly braces here since we are inside JSX. We are using the map method on every item(campsite) in the campsites array of objects and we want the arrow function to return the stated properties of each one of the items. Map returns a new array. It will be rendered bellow where we added the directory variable inside the div with className="row".*/}
-                    <h2>{campsite.name}</h2>
-                    <p>{campsite.description}</p>
+                <Card>
+                    <CardImg top src = {campsite.image} alt = {campsite.name} />
+                    <CardBody>
+                        <CardTitle>{campsite.name}</CardTitle>
+                        <CardText>{campsite.description}</CardText>
+                    </CardBody>
+                </Card>
+            )
+        }
+        /*Setting up a return bellow the if block in case we didn't make it into the block if evaluated as falsy. It will return a simple empty <div> */
+        return <div />;
+    }
+
+     render() {
+        const directory = this.props.campsites.map(campsite => {
+            return (
+                <div key = {campsite.id} className = "col-md-5 m-1">           {/*Here we are using key so we can identify each item in the array of objects by it's unic id. The app will work fine without it but it gives warning in the console and we add it to follow best practices*/}
+                    <Card onClick={() => this.onCampsiteSelect(campsite)}>
+                        <CardImg src = {campsite.image} alt = {campsite.name} />        {/*We are using curly braces here since we are inside JSX. We are using the map method on every item(campsite) in the campsites array of objects and we want the arrow function to return the stated properties of each one of the items. Map returns a new array. It will be rendered bellow where we added the directory variable inside the div with className="row".*/}
+                        <CardImgOverlay>
+                            <CardTitle>{campsite.name}</CardTitle>
+                        </CardImgOverlay>
+                    </Card>
                 </div>
             );
         });
-
+/*The bellow return is the return from the whole component. When we are ready to break out of this component and send some date back to the parent component-that is going to happend exactly in this final return statment. All the other returns above were inside the methods which are contained inside the DirectoryComponent. These methods are just being used to pass date inside the component. */
         return (
             <div className = "container">
                 <div className = "row">
                     {directory}                           {/*Adding a JS variable that we set up after render(). It must be inside curly braces. This directory variable and the Directory class are two separate things.*/}
+                </div>
+                <div className = "row">
+                    <div className = "col-md-5 m-1">
+                        {this.renderSelectedCampsite(this.state.selectedCampsite)}
+                    </div>
                 </div>
             </div>
         );
