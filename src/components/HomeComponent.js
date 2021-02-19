18db824b171b49303edcae897c40276b56191ebb
Mariya Ivanova <m80ivanova@gmail.com>
2021-02-18 21:27:11 -0600

    React Animation Components

--- src/components/HomeComponent.js
+++ src/components/HomeComponent.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Card, CardImg, CardText, CardBody, CardTitle } from 'reactstrap';
 import { Loading }  from "./LoadingComponent";
 import { baseUrl} from "../shared/baseUrl";
+import { FadeTransform } from "react-animation-components";
 
 //We are adding isLoading and errMess as props and are creating an if statement in RenderCard below to check if isLoading is truthy, if it is it will return the Loading component so the spinner will show up with the message "Loading..."
 //If errMess is truthy we will return the context of the error message.
@@ -13,14 +14,21 @@ function RenderCard({item, isLoading, errMess}) {
     if(errMess) {
         return <h4>{errMess}</h4>
     }
+    //The "in" attribute is boolean and it tells the component to run this transition when beeing mounted. The transformProps sets the transition from the initial scale of 50% to the normal value so it will create a pop effect. With translateY we will have the component move vertically as well.
     return (
-        <Card>
-            <CardImg src={baseUrl + item.image} alt = {item.name} />
-            <CardBody>
-                <CardTitle>{item.name}</CardTitle>
-                <CardText>{item.description}</CardText>
-            </CardBody>
-        </Card>
+            <FadeTransform
+                in
+                transformProps={{
+                    exitTransform: "scale(0.5) translateY(50%)"
+             }} >
+                <Card>
+                    <CardImg src={baseUrl + item.image} alt = {item.name} />
+                    <CardBody>
+                        <CardTitle>{item.name}</CardTitle>
+                        <CardText>{item.description}</CardText>
+                    </CardBody>
+                </Card>
+        </FadeTransform>
     );
 }
 
